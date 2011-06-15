5f35917c7e15915bafaa70a394e0dae3e216c404
Robert Righter <robrighter@gmail.com>
2011-06-15 19:20:02 -0400

    Added back in grappelli

--- host_settings/webfaction.py
+++ host_settings/webfaction.py
@@ -11,6 +11,29 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'ncfmusic.apps.content',
+    'ncfmusic.apps.heroshots',
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
 
 
 
